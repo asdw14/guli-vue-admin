e421e64648f9f1a1e586362573399b85cbd1b237
dizhongdi <2755063993@qq.com>
2022-06-23 00:31:31 +0800

    课程大纲列表增删改查

--- src/api/edu/chapter.js
+++ src/api/edu/chapter.js
@@ -12,6 +12,41 @@ export default {
             }
         })
 
+    },
+
+    //添加章节
+    save(chapter){
+        return request({
+            url: `${api_name}/`,
+            method : "post",
+            data: chapter
+        })
+    },
+
+    //删除章节
+    removeById(id){
+        return request({
+            url: `${api_name}/${id}`,
+            method : "delete",
+        })
+    },
+    
+    //修改章节
+    updateById(chapter){
+        return request({
+            url: `${api_name}/${chapter.id}`,
+            method : "put",
+            data: chapter
+        })
+    },
+
+    //根据id查章节
+    getById(id) {
+        return request({
+            url: `${api_name}/${id}`,
+            method : "get",
+        })
     }
+
 
 }
--- src/views/edu/course/chapter.vue
+++ src/views/edu/course/chapter.vue
@@ -2,7 +2,22 @@
 
   <div class="app-container">
 
-    <el-button type="text">添加章节</el-button>
+    <el-button type="text" @click="dialogChapterFormVisible = true">添加章节</el-button>
+    <!-- 添加和修改章节表单 -->
+    <el-dialog :visible.sync="dialogChapterFormVisible" title="添加章节">
+        <el-form :model="chapter" label-width="120px">
+            <el-form-item label="章节标题">
+                <el-input v-model="chapter.title"/>
+            </el-form-item>
+            <el-form-item label="章节排序">
+                <el-input-number v-model="chapter.sort" :min="0" controls-position="right"/>
+            </el-form-item>
+        </el-form>
+        <div slot="footer" class="dialog-footer">
+            <el-button @click="dialogChapterFormVisible = false">取 消</el-button>
+            <el-button type="primary" @click="saveOrUpdate">确 定</el-button>
+        </div>
+    </el-dialog>
 <!-- 章节 -->
 <ul class="chanpterList">
     <li
@@ -13,8 +28,8 @@
 
             <span class="acts">
                 <el-button type="text">添加课时</el-button>
-                <el-button style="" type="text">编辑</el-button>
-                <el-button type="text">删除</el-button>
+                <el-button type="text" @click="editChapter(chapter.id)">编辑</el-button>
+                <el-button type="text" @click="removeChapter(chapter.id)">删除</el-button>
             </span>
         </p>
 
@@ -46,7 +61,13 @@ export default {
     return {
       saveBtnDisabled: false, // 保存按钮是否禁用
       courseId: '', // 所属课程id
-      chapterNestedList: [] // 章节嵌套课时列表
+      chapterNestedList: [], // 章节嵌套课时列表、
+      dialogChapterFormVisible: false, //是否显示章节表单
+      chapter: {// 章节对象
+        id:'',
+        title: '',
+        sort: 0
+      }
     }
   },
 
@@ -67,9 +88,97 @@ export default {
     fetchChapterNestedListByCourseId(){
       chapter.getNestedTreeList(this.courseId).then(resopnse =>{
         this.chapterNestedList = resopnse.data.items
-})
+      })
 
-    },      
+    },
+    saveOrUpdate() {
+      this.saveBtnDisabled = true
+        if (!this.chapter.id) {
+          this.saveData()
+        } else {
+          this.updateData()
+        }
+    },    
+
+    saveData(){
+        this.chapter.courseId = this.courseId
+        chapter.save(this.chapter).then(response =>{
+              this.$message({
+                type: 'success',
+                message: '保存成功!'
+              })
+            //清空表单以及刷新        
+            this.helpSave()
+        }).catch((response) => {
+          this.$message({
+            type: 'error',
+            message: response.message
+          })
+      })
+    },
+    removeChapter(chapterId){
+      this.$confirm('此操作将永久删除该记录, 是否继续?', '提示', {
+    confirmButtonText: '确定',
+    cancelButtonText: '取消',
+    type: 'warning'
+  }).then(() => {
+    return chapter.removeById(chapterId)
+  }).then(() => {
+    this.fetchChapterNestedListByCourseId()// 刷新列表
+    this.$message({
+      type: 'success',
+      message: '删除成功!'
+    })
+  }).catch((response) => { // 失败
+    if (response === 'cancel') {
+      this.$message({
+        type: 'info',
+        message: '已取消删除'
+      })
+    } else {
+      this.$message({
+        type: 'error',
+        message: response.message
+      })
+    }
+  })
+    },
+
+
+
+// 修改章节 
+    editChapter(chapterId) {
+    this.dialogChapterFormVisible = true
+    chapter.getById(chapterId).then(response => {
+        this.chapter = response.data.item
+    })
+},
+    updateData() {
+      this.chapter.courseId = this.courseId
+        chapter.updateById(this.chapter).then(response =>{
+              this.$message({
+                type: 'success',
+                message: '修改成功!'
+              })
+            //清空表单以及刷新        
+            this.helpSave()
+        }).catch((response) => {
+          this.$message({
+            type: 'error',
+            message: response.message
+          })
+      })
+    },
+
+    // 清空表单刷新页面关闭添加章节弹窗
+    helpSave(){
+      this.dialogChapterFormVisible = false// 如果保存成功则关闭对话框
+      this.chapter.title = ''// 重置章节标题
+      this.chapter.sort = ''
+      this.fetchChapterNestedListByCourseId()// 刷新列表
+
+
+    },
 
     previous() {
       console.log('previous')
